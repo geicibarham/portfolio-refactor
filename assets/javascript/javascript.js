4fe596e96cb37567a99d9923673a4ab60c5cb9cf
geicibarham <geicianecosta43@gmail.com>
2022-04-24 11:53:48 -0400

    edit footer

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,59 +1,59 @@
 
 
-// async function typeSentence(sentence, eleRef, delay = 100) {
-//     const letters = sentence.split("");
-//     let i = 0;
-//     while(i < letters.length) {
-//       await waitForMs(delay);
-//       $(eleRef).append(letters[i]);
-//       i++
-//     }
-//     return;
-//   }
+async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      $(eleRef).append(letters[i]);
+      i++
+    }
+    return;
+  }
   
   
-//   function waitForMs(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms))
-//   }
+  function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 
-//   typeSentence("Welcome To My Website!", "#sentence")
+  typeSentence("Welcome To My Website!", "#sentence")
 
   
-// async function deleteSentence(eleRef) {
-//     const sentence = $(eleRef).html();
-//     const letters = sentence.split("");
-//     let i = 0;
-//     while(letters.length > 0) {
-//       await waitForMs(400);
-//       letters.pop();
-//       $(eleRef).html(letters.join(""));
-//     }
-//   }
+async function deleteSentence(eleRef) {
+    const sentence = $(eleRef).html();
+    const letters = sentence.split("");
+    let i = 0;
+    while(letters.length > 0) {
+      await waitForMs(400);
+      letters.pop();
+      $(eleRef).html(letters.join(""));
+    }
+  }
 
-//   $( document ).ready(async function() {
-//     await typeSentence("", "#sentence");
-//     await waitForMs(4000);
-//     deleteSentence("#sentence");
-//   });
+  $( document ).ready(async function() {
+    await typeSentence("", "#sentence");
+    await waitForMs(4000);
+    deleteSentence("#sentence");
+  });
   
 
-//   async function typeSentence(sentence, eleRef, delay = 100) {
-//     const letters = sentence.split("");
-//     let i = 0;
-//     while(i < letters.length) {
-//       await waitForMs(delay);
-//       $(eleRef).append(letters[i]);
-//       i++
-//     }
-//     return;
-//   }
+  async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      $(eleRef).append(letters[i]);
+      i++
+    }
+    return;
+  }
   
   
-//   function waitForMs(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms))
-//   }
+  function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 
-//   typeSentence("", "#sentence")
+  typeSentence("", "#sentence")
 
 
 window.onscroll = function() {scrollFunction()};
